849d0d38ee0cfe20b20e08b318d974b2bef0f982
hmarinho <rm558841@fiap.com.br>
2025-03-06 16:56:57 -0300

    commit1

--- src/main/java/br/com/fiap/beach_play_api/controller/ReservationController.java
+++ src/main/java/br/com/fiap/beach_play_api/controller/ReservationController.java
@@ -2,15 +2,39 @@ package br.com.fiap.beach_play_api.controller;
 
 import br.com.fiap.beach_play_api.model.Reservation;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/reservations") //definindo as rotas
 public class ReservationController {
-    @RequestMapping(path="/home",produces="application/json",method = {RequestMethod.GET})
-	public Reservation index(){
-		return new Reservation(null, 0, null, null);
+
+	private List<Reservation> repository = new ArrayList<>(List.of(
+        new Reservation(1L, 1, LocalDate.of(2025, 3, 01), LocalTime.of(10, 00)) 
+		//coloquei esses mocks de teste para essa lista
+    ));
+
+	// Método Get - Retorna as reservas cadastradas.
+	@GetMapping
+	public List<Reservation> index() {
+		return repository; //retorna a lista criada
 	}
-    
+
+	// Método Post - Para criar uma reserva.
+	@PostMapping
+	public ResponseEntity<Reservation> create(@RequestBody Reservation reservation){  
+		//recebeu a requisição com requestbody e converteu para o objeto reservation
+		System.out.println("Cadastrando a reserva na quadra: " + reservation.getQuadra());
+		repository.add(reservation);
+		return ResponseEntity.status(201).body(reservation);
+	}
 }
--- src/main/java/br/com/fiap/beach_play_api/model/Reservation.java
+++ src/main/java/br/com/fiap/beach_play_api/model/Reservation.java
@@ -7,15 +7,15 @@ import java.util.Random;
 public class Reservation {
     private Long id;
     private int quadra;
-    private LocalDate data;
-    private LocalTime horario;
+    private LocalDate date;
+    private LocalTime time;
 
     //  Contrutores
     public Reservation(Long id, int quadra, LocalDate data, LocalTime horario) {
         this.id = Math.abs(new Random().nextLong()); //aleatorizar um id
         this.quadra = quadra;
-        this.data = data;
-        this.horario = horario;
+        this.date = date;
+        this.time = time;
     }
     //  Getters (obter valor, permite visualizar, ler o valor do atributo)
     //  Setters (modificar o valor)
@@ -31,17 +31,17 @@ public class Reservation {
     public void setQuadra(int quadra) {
         this.quadra = quadra;
     }
-    public LocalDate getData() {
-        return data;
+    public LocalDate getDate() {
+        return date;
     }
-    public void setData(LocalDate data) {
-        this.data = data;
+    public void setDate(LocalDate date) {
+        this.date = date;
     }
-    public LocalTime getHorario() {
-        return horario;
+    public LocalTime getTime() {
+        return time;
     }
-    public void setHorario(LocalTime horario) {
-        this.horario = horario;
+    public void setHorario(LocalTime time) {
+        this.time = time;
     }
     
 }
