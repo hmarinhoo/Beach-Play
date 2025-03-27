8427e97148ba298fb56d11f9fbeb64d89bb7211e
hmarinho <rm558841@fiap.com.br>
2025-03-27 01:42:38 -0300

    Config. para conectar com web

---
+++ src/main/java/br/com/fiap/beach_play_api/config/WebConfig.java
@@ -0,0 +1,17 @@
+package br.com.fiap.beach_play_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Permite CORS para todas as rotas
+                .allowedOrigins("http://localhost:3000")  // Adiciona a origem do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos permitidos
+                .allowedHeaders("*");  // Permite todos os cabeçalhos
+    }
+}
