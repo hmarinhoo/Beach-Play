af309294420a154d26277b5ea213f64d8d86ed29
hmarinho <rm558841@fiap.com.br>
2025-04-03 09:05:10 -0300

    teste teste

---
+++ src/main/java/br/com/fiap/beach_play_api/repository/AppRepository.java
@@ -0,0 +1,13 @@
+package br.com.fiap.beach_play_api.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRepository {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRepository.class, args);
+	}
+
+}
